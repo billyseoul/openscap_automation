0ec4dfa0ab97d0bb3e02ad9b328a34279651022b
Billy Campbell <lee.min.seoul@gmail.com>
2023-06-17 16:01:37 -0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "region" {
+  type = string
+  default = "us-east-1"
+}
+
+variable "ami" {
+  type = string
+  default = "ami-0b4490a1f01247101"
+}
+
+variable "key_name" {
+  type = string
+  default = "add key"
+}
